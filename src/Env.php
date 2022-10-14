d3b4fe4885b9f9db0960917df6c15ce6aeb2420e
Kevin Traini <kevin@ktraini.com>
2022-10-14 13:52:55 +0200

    Create Env class + Env::load()

---
+++ src/Env.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Gashmob\Dotenv;
+
+use Gashmob\Dotenv\exceptions\FileNotFoundException;
+
+$_ENV = [];
+
+class Env
+{
+    /**
+     * Load a .env file and store it to $_ENV and $_SERVER['ENV']
+     *
+     * @param string $file
+     * @return void
+     * @throws FileNotFoundException
+     */
+    public static function load($file = __DIR__ . '/.env')
+    {
+        if (!file_exists($file)) {
+            throw new FileNotFoundException($file);
+        }
+
+        $content = file_get_contents($file);
+
+        $lines = explode(PHP_EOL, $content);
+
+        global $_ENV;
+        $server = [];
+        foreach ($lines as $line) {
+            if (empty($line)) {
+                continue;
+            }
+
+            $line = trim($line);
+
+            if (strpos($line, '#') === 0) {
+                continue;
+            }
+
+            $parts = explode('=', $line, 2);
+            assert(count($parts) === 2);
+            $key = trim($parts[0]);
+            $value = trim($parts[1]);
+
+            $_ENV[$key] = $value;
+            $server[$key] = $value;
+        }
+        $_SERVER['ENV'] = $server;
+    }
+}
